d4b192aec58e4c52646537c525713a009ffa5e79
beginner <84720129+grand3680@users.noreply.github.com>
2023-05-15 21:50:23 +0300

    Update js.js

--- Sourse/js/js.js
+++ Sourse/js/js.js
@@ -1,5 +1,5 @@
 $("#__Scroll_Down__").click(function() {
     $('html, body').animate({
-        scrollTop: $(".footer").offset().top // класс объекта к которому приезжаем
+        scrollTop: $(".footer").offset().top // the class of the object we are arriving at
     }, 2500);
 });
